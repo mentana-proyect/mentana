3e32dca3562bea7c365fe54da43dca7af93aaa0a
Mentana <mentanacl@gmail.com>
2025-09-30 10:51:42 -0300

    commit initial

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import { supabase } from '@/lib/supabaseClient';
 import { useRouter } from 'next/navigation';
 
+export const dynamic = "force-dynamic"; 
+
 export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
