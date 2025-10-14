b83d5363fd545175dc2d6031470ae30acfc5d43f
Mentana <mentanacl@gmail.com>
2025-10-14 17:59:04 -0300

    commit initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,6 +55,19 @@ export default function HomePage() {
           <p>
             <strong>Únete al viaje.</strong>
           </p>
+         <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="22"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <polyline points="8 12 16 20 24 12" />
+        </svg>
+
         </section>
 
         {/* Botones de acción */}
