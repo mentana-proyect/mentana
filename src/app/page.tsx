37c3bcedce244498418f5b3b0f27f1b77834b34d
Mentana <mentanacl@gmail.com>
2025-10-14 18:58:18 -0300

    commit initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,6 +55,7 @@ export default function HomePage() {
           <p>
             <strong>Únete al viaje.</strong>
           </p>
+          
          <svg
           xmlns="http://www.w3.org/2000/svg"
           width="32"
