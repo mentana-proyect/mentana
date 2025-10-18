aabd63e7b8b332d277107e6459d80800b1285c0b
Mentana <mentanacl@gmail.com>
2025-10-17 23:56:14 -0300

    commit initial

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
-import "../../../styles/modal.css";
+import "../styles/modal.css";
 
 interface ModalProps {
   isOpen: boolean;
