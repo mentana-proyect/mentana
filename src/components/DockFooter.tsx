9ea65f3079a7784df363a5bb5c47b7199279db2e
Mentana <mentanacl@gmail.com>
2025-10-21 02:35:25 -0300

    commit initial

--- src/components/DockFooter.tsx
+++ src/components/DockFooter.tsx
@@ -8,6 +8,22 @@ interface Props {
 const DockFooter: React.FC<Props> = ({ logout }) => (
   <footer className="dock-footer">
     <nav className="dock">
+      <a href="./setting" title="Configuración">
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        stroke="white"
+        fill="none"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="feather feather-settings"
+      >
+        <circle cx="12" cy="12" r="3" />
+        <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82 2 2 0 0 1-2.83 2.83 1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51v0a2 2 0 0 1-4 0v0a1.65 1.65 0 0 0-1-1.51 1.65 1.65 0 0 0-1.82.33 2 2 0 0 1-2.83-2.83 1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1 0-4h0a1.65 1.65 0 0 0 1.51-1 1.65 1.65 0 0 0-.33-1.82A2 2 0 0 1 7 4.6a1.65 1.65 0 0 0 1.82.33h0A1.65 1.65 0 0 0 10 3.42V3a2 2 0 0 1 4 0v0a1.65 1.65 0 0 0 1 1.51h0a1.65 1.65 0 0 0 1.82-.33A2 2 0 0 1 19.4 7a1.65 1.65 0 0 0-.33 1.82h0A1.65 1.65 0 0 0 20.58 10H21a2 2 0 0 1 0 4h0a1.65 1.65 0 0 0-1.51 1z" />
+      </svg>
+      </a>
       <a href="./home" title="Inicio">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="white" fill="none"
              strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
@@ -18,14 +34,7 @@ const DockFooter: React.FC<Props> = ({ logout }) => (
         </svg>
       </a>
 
-      <a href="./setting" title="Configuración">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="white" fill="none"
-             strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-             className="feather feather-settings">
-          <circle cx="12" cy="12" r="3" />
-          <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82..." />
-        </svg>
-      </a>
+      
 
       <a onClick={logout} title="Cerrar sesión" style={{ background: "none", border: "none", cursor: "pointer" }}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="white" fill="none"
