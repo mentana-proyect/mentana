e01bb59c981500ec928f1d52398bfb02845ffdf0
Mentana <mentanacl@gmail.com>
2025-10-27 17:58:16 -0300

    commit initial

--- src/components/QuizList.tsx
+++ src/components/QuizList.tsx
@@ -1,8 +1,7 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import QuizCard from "./QuizCard";
 import { Category } from "../components/useProgress";
-import { supabase } from "../lib/supabaseClient";
 
 interface QuizListProps {
   categories: Category[];
@@ -12,7 +11,6 @@ interface QuizListProps {
 }
 
 export default function QuizList({ categories, openModal, openResult, openRecomendacion }: QuizListProps) {
-
   return (
     <div className="quiz-list grid gap-4">
       {categories.map((cat, index) => (
